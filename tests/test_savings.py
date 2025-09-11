cd42765deaef5535fa9294e6aed4610f196a42d7
Anthony Berg <smyalygames@gmail.com>
2025-09-11 12:30:05 +0200

    test(bank): add test for apply interest

---
+++ tests/test_savings.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from bank import SavingsAccount
+
+
+class TestSavingsAccount(TestCase):
+    def setUp(self):
+        self.name = "John Doe"
+        self.interest = 0.05
+        self.account = SavingsAccount(self.name, self.interest)
+        self.balance = 100000
+        self.account.deposit(self.balance)
+
+    def test_apply_interest(self):
+        expected_balance = self.balance * (1 + self.interest)
+        self.account.apply_interest()
+
+        self.assertEqual(self.account.account_info().balance, expected_balance, "The expected balance is not the same what is on the account.")

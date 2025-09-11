9a38d5779dcb7f71a9f23a47da53d7012981a953
Anthony Berg <smyalygames@gmail.com>
2025-09-11 10:13:03 +0200

    feat(bank): add SavingsAccount class

--- bank/savings.py
+++ bank/savings.py
@@ -1,10 +1,21 @@
-class ElectricCar:
-    def fuel_type(self):
-        return "Electric"
-class GasCar:
-    def fuel_type(self):
-        return "Gasoline"
-class HybridCar(ElectricCar, GasCar):
-    pass
-car = HybridCar()
-print(car.fuel_type())
+from bank.bank import BankAccount
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self, name: str, interest_rate: float):
+        """
+
+        :param name: Name of the account holder
+        :param interest_rate: The interest rate as a percentage in decimal form. e.g. 2% interest is 0.02
+        """
+        super().__init__(name)
+
+        # TODO maybe add a check if the interest rate is valid? i.e. not negative.
+
+        self.interest_rate = interest_rate
+
+    def apply_interest(self) -> None:
+        """
+        Applies the account's interest rate on the account's balance
+        """
+        self.balance = self.balance * (1 + self.interest_rate)

f0534249002f5a1344251a37333a9527589621a2
Anthony Berg <smyalygames@gmail.com>
2025-09-11 10:16:26 +0200

    feat(bank): add CheckingAccount class

---
+++ bank/checking.py
@@ -0,0 +1,23 @@
+from bank.bank import BankAccount
+
+
+class CheckingAccount(BankAccount):
+    def __init__(self, name: str, fee: int):
+        """
+
+        :param name: Name of the account holder
+        :param fee: Fixed fee for withdrawals
+        """
+        super().__init__(name)
+
+        if fee < 0:
+            raise ValueError("The fee cannot be negative.")
+
+        self.fee = fee
+
+    def withdraw(self, amount: int) -> None:
+        """
+        Withdraws the amount specified from the account, with the additional fee.
+        :param: The amount to withdraw, excludes the fee
+        """
+        super().withdraw(amount + self.fee)

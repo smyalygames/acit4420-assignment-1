28447a9ba58e92ad3966ef7b7f09a1611cda3cb7
Anthony Berg <smyalygames@gmail.com>
2025-09-18 11:28:25 +0200

    feat(test): add invalid interest rate test

--- tests/test_savings.py
+++ tests/test_savings.py
@@ -13,6 +13,26 @@ class TestSavingsAccount(TestCase):
 
     def test_apply_interest(self):
         expected_balance = self.balance * (1 + self.interest)
-        self.account.apply_interest()
+        expected_interest = expected_balance - self.balance
 
-        self.assertEqual(expected_balance, self.account.account_info().balance, "The expected balance is not the same what is on the account.")
+        # Apply interest to the account
+        interest_gained = self.account.apply_interest()
+
+        # Gets the balance in the account after applying the interest
+        actual_balance = self.account.account_info().balance
+
+        # Checks the amount of interest gained
+        self.assertEqual(expected_interest, interest_gained, "Interest gained is not as expected.")
+
+        # Checks the balance of the account is correct
+        self.assertEqual(expected_balance, actual_balance,
+                         "Balance in the account is not the same as the expected balance.")
+
+    def test_invalid_interest(self):
+        # Can't have a negative interest rate
+        with self.assertRaises(ValueError):
+            SavingsAccount(self.name, interest_rate=-0.55)
+
+        # Cannot have zero interest rate
+        with self.assertRaises(ValueError):
+            SavingsAccount(self.name, interest_rate=0.0)

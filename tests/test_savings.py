5f3dfbb6bca32e6fd756f4e7a9bfda8671686996
Anthony Berg <smyalygames@gmail.com>
2025-09-20 23:16:58 +0200

    feat(test): add test for applying interest on a balance with nothing

--- tests/test_savings.py
+++ tests/test_savings.py
@@ -12,7 +12,7 @@ class TestSavingsAccount(TestCase):
         self.account.deposit(self.balance)
 
     def test_apply_interest(self):
-        expected_balance = self.balance * (1 + self.interest)
+        expected_balance = round(self.balance * (1 + self.interest))
         expected_interest = expected_balance - self.balance
 
         # Apply interest to the account
@@ -22,11 +22,35 @@ class TestSavingsAccount(TestCase):
         actual_balance = self.account.account_info().balance
 
         # Checks the amount of interest gained
-        self.assertEqual(expected_interest, interest_gained, "Interest gained is not as expected.")
+        self.assertEqual(
+            expected_interest, interest_gained, "Interest gained is not as expected."
+        )
 
         # Checks the balance of the account is correct
-        self.assertEqual(expected_balance, actual_balance,
-                         "Balance in the account is not the same as the expected balance.")
+        self.assertEqual(
+            expected_balance,
+            actual_balance,
+            "Balance in the account is not the same as the expected balance.",
+        )
+
+    def test_apply_interest_zero_balance(self):
+        # Removes the balance from setUp()
+        self.account.withdraw(self.balance)
+
+        self.assertEqual(
+            0, self.account.balance, "Balance should be empty before starting the test."
+        )
+
+        interest_gained = self.account.apply_interest()
+
+        self.assertEqual(
+            0,
+            interest_gained,
+            "No interest should have been given for an empty balance.",
+        )
+        self.assertEqual(
+            0, self.account.balance, "Balance should be empty after applying interest."
+        )
 
     def test_invalid_interest(self):
         # Can't have a negative interest rate
